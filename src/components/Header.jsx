f12adefc0b7849fa621a4e543499c0c81d49725a
eluluchan <tkehy11@gmail.com>
2022-08-27 18:56:08 +0900

    Update: profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,18 +14,18 @@ export const Header = () => {
             {/* カバー画像 */}
             <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
             {/* 名前と肩書はみなさんのお名前や肩書を自由に入れてください */}
-            <h1 className="title-text">あなたの名前</h1>
-            <h3 className="title-text">あなたの肩書(ex.Engineer)</h3>
+            <h1 className="title-text">eluluchan</h1>
+            <h3 className="title-text">Engineer</h3>
             <ul className="social-icons">
             <li className="icon-link">
                 {/* リンク先(href)は任意のURLを設定してください */}
-                <a href="https://twitter.com/">
+                <a href="https://twitter.com/kurosustreet">
                   <FaTwitter color="white" size="2rem" />
                 </a>
               </li>
               <li className="icon-link">
                 {/* リンク先(href)は任意のURLを設定してください */}
-                <a href="https://github.com/">
+                <a href="https://github.com/eluluchan">
                   <FaGithub color="white" size="2rem" />
                 </a>
               </li>
